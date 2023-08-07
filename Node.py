6fc53cb9bfe6123aad35d00fc3d04003c4ab8e10
ahnafakif <48433905+ahnaf-akif@users.noreply.github.com>
2023-08-06 21:55:19 -0700

    Add files via upload

---
+++ Node.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, x, y, parent=None):
+        self.x = x
+        self.y = y
+        self.parent = parent
+
+    def set_x(self, x):
+        self.x = x
+
+    def set_y(self, y):
+        self.y = y
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def set_parent(self, parent):
+        self.parent = parent
+
+    def get_parent(self):
+        return self.parent
